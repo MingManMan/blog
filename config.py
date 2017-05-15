c3720cbe429c447b723a72297cd5fed3c2ab4189
MingManMan <133547@qq.com>
2017-05-15 23:05:46 +0800

    5a 5.7
    5.7 5.8 章节对应代码

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
+import os 
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
--- hello.py
+++ hello.py
@@ -5,19 +5,39 @@ from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
-from flask_wtf import Form
-from wtforms import StringField,SubmitField
-from wtforms.validators import Required
-
-class NameForm(Form):
-	name=StringField("What is your name?",validators=[Required()])
-	submit=SubmitField(u'提交')
-
+import os
+from flask_sqlalchemy import SQLAlchemy
+basedir=os.path.abspath(os.path.dirname(__file__))
 app=Flask(__name__)
+
+app.config.from_object('config')
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///'+os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+db=SQLAlchemy(app)
+
+
 bootstrap=Bootstrap(app)
 moment=Moment(app)
 manager=Manager(app)
-app.config.from_object('config')
+
+class Role(db.Model):
+	"""docstring for Role"""
+	__tablename__='roles'
+	id=db.Column(db.Integer,primary_key=True)
+	name=db.Column(db.String(64),unique=True)
+	users=db.relationship('User',backref='role')
+	def __repr__(self):
+		return '<Role %r>'%self.name
+class User(db.Model):
+	__tablename__='users'
+	id=db.Column(db.Integer,primary_key=True)
+	username=db.Column(db.String(64),unique=True,index=True)
+	role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+	def __repr__(self):
+		return "<User %r>"%self.usernanme
+		
+
+
 @app.route('/',methods=['GET','POST'])
 def index():
 	name=None
