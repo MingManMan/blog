96523df8bc973ee06fa51345d00a8b39838ae52a
MingManMan <133547@qq.com>
2017-05-15 21:20:56 +0800

    3.1.1
    简单模板

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 #coding=utf-8
-from flask import Flask
+from flask import Flask,render_template
 from flask import request,redirect,abort
 from flask_script import Manager
 app=Flask(__name__)
@@ -7,7 +7,10 @@ manager=Manager(app)
 
 @app.route('/')
 def index():
-	user_agent=request.headers.get('User-Agent')
-	return "<p>Your browser is %s"%user_agent
+	return render_template('index.html')
+
+@app.route('/user/<username>')
+def user(username):
+	return render_template('index.html',name=username)
 if __name__=='__main__':
 	manager.run()
