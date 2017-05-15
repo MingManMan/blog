7258adad657c4ad01b073ebaf4d4b621c7e881ce
MingManMan <133547@qq.com>
2017-05-15 22:28:06 +0800

    4b 4.5
    章节4.5  简单表单和重定向

--- hello.py
+++ hello.py
@@ -1,31 +1,34 @@
 #coding=utf-8
 from flask import Flask,render_template
-from flask import request,redirect,abort
+from flask import request,redirect,abort,session,redirect,url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
+
+class NameForm(Form):
+	name=StringField("What is your name?",validators=[Required()])
+	submit=SubmitField(u'提交')
 
 app=Flask(__name__)
 bootstrap=Bootstrap(app)
 moment=Moment(app)
 manager=Manager(app)
-
-@app.route('/')
+app.config.from_object('config')
+@app.route('/',methods=['GET','POST'])
 def index():
-	return render_template('index.html',current_time=datetime.utcnow())
+	name=None
+	form=NameForm()
+	if form.validate_on_submit():
+		name=form.name.data
+		session['name']=name
+		form.name.data=''
+		return redirect(url_for('index'))
+	return render_template('index.html',form=form,name=session.get('name'))
 
-@app.errorhandler(404)
-def page_not_found(e):
-	return render_template('404.html'),404
 
-@app.errorhandler(500)
-def internal_server_error(e):
-	return render_template('500.html'),500
-	
-
-@app.route('/user/<username>')
-def user(username):
-	return render_template('user.html',name=username)
 if __name__=='__main__':
 	manager.run()
