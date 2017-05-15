01cebfdccd582703bc86cb1856227f7eb3eec2bb
MingManMan <133547@qq.com>
2017-05-15 21:15:45 +0800

    2.6
    命令行支持

--- hello.py
+++ hello.py
@@ -1,23 +1,13 @@
 #coding=utf-8
 from flask import Flask
 from flask import request,redirect,abort
+from flask_script import Manager
 app=Flask(__name__)
+manager=Manager(app)
+
 @app.route('/')
 def index():
 	user_agent=request.headers.get('User-Agent')
 	return "<p>Your browser is %s"%user_agent
-@app.route("/user/<name>")
-def user(name):
-	return "<h1>Hello,%s!"%name
-@app.route("/400/")
-def my400():
-	return '<h1>Bad Request</h1>',400
-@app.route("/redir/<url>")
-def redir(url):
-	return redirect("http://"+url)
-
-@app.route("/get_user/<id>")
-def get_user(id):
-	abort(404)
 if __name__=='__main__':
-	app.run(debug=True)
+	manager.run()
