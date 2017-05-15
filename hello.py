c7f5b027aff4dc3fefd63a21903e55cb005e3453
MingManMan <133547@qq.com>
2017-05-15 21:37:29 +0800

    3.2 3b
    bootstrap 模板使用

--- hello.py
+++ hello.py
@@ -2,7 +2,10 @@
 from flask import Flask,render_template
 from flask import request,redirect,abort
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
+
 app=Flask(__name__)
+bootstrap=Bootstrap(app)
 manager=Manager(app)
 
 @app.route('/')
