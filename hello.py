18e6b9bc1222589195dd63dfe0655588c9621bab
MingManMan <133547@qq.com>
2017-05-15 22:10:14 +0800

    3e 3.6
    3.6章节 3e版本

--- hello.py
+++ hello.py
@@ -3,14 +3,17 @@ from flask import Flask,render_template
 from flask import request,redirect,abort
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app=Flask(__name__)
 bootstrap=Bootstrap(app)
+moment=Moment(app)
 manager=Manager(app)
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html',current_time=datetime.utcnow())
 
 @app.errorhandler(404)
 def page_not_found(e):
