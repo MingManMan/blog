aedd5f66a3d6d6b0d40768b6889ceb6b3ab711f5
MingManMan <133547@qq.com>
2017-05-16 21:10:02 +0800

    5b  5.9
    章节5.9  5b版本

--- hello.py
+++ hello.py
@@ -1,12 +1,17 @@
 #coding=utf-8
 from flask import Flask,render_template
-from flask import request,redirect,abort,session,redirect,url_for,flash
+from flask import request,redirect,abort
+from flask import request,redirect,abort,session,redirect,url_for
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 import os
 from flask_sqlalchemy import SQLAlchemy
+
 basedir=os.path.abspath(os.path.dirname(__file__))
 app=Flask(__name__)
 
@@ -25,7 +30,7 @@ class Role(db.Model):
 	__tablename__='roles'
 	id=db.Column(db.Integer,primary_key=True)
 	name=db.Column(db.String(64),unique=True)
-	users=db.relationship('User',backref='role')
+	users=db.relationship('User',backref='role',lazy='dynamic')
 	def __repr__(self):
 		return '<Role %r>'%self.name
 class User(db.Model):
@@ -33,22 +38,30 @@ class User(db.Model):
 	id=db.Column(db.Integer,primary_key=True)
 	username=db.Column(db.String(64),unique=True,index=True)
 	role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
-	def __repr__(self):
-		return "<User %r>"%self.usernanme
-		
+	def __repr_(self):
+		return "<User %r>"%self.username
+
+class NameForm(Form):
+	name=StringField("What is your name?",validators=[Required()])
+	submit=SubmitField(u'提交')		
 
 
 @app.route('/',methods=['GET','POST'])
 def index():
-	name=None
 	form=NameForm()
 	if form.validate_on_submit():
-		old_name=session.get('name')
-		if old_name is not None and old_name!=form.name.data:
-			flash('Look like you have changed your name!')
+		user=User.query.filter_by(username=form.name.data).first()
+		if user is None:
+			user=User(username=form.name.data)
+			db.session.add(user)
+			session['known']=False
+		else:
+			session['known']=True
 		session['name']=form.name.data
+		form.name.data=''
 		return redirect(url_for('index'))
-	return render_template('index.html',form=form,name=session.get('name'))
+	return render_template('index.html',form=form,name=session.get('name'),known=session.get('known',False))
+
 
 
 if __name__=='__main__':
