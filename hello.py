dde065ca0d14d035230ab9f216873795bc266133
mingmanman <133547@qq.com>
2017-05-17 09:51:21 +0800

    放弃这个更新
    放弃的更新

--- hello.py
+++ hello.py
@@ -40,16 +40,7 @@ class User(db.Model):
 
 @app.route('/',methods=['GET','POST'])
 def index():
-	name=None
-	form=NameForm()
-	if form.validate_on_submit():
-		old_name=session.get('name')
-		if old_name is not None and old_name!=form.name.data:
-			flash('Look like you have changed your name!')
-		session['name']=form.name.data
-		return redirect(url_for('index'))
-	return render_template('index.html',form=form,name=session.get('name'))
-
+	return "hello"
 
 if __name__=='__main__':
 	manager.run()
