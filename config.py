b9453d7fa906bf6440689c12bdf1bd3e0a325acd
MingManMan <133547@qq.com>
2017-05-29 15:24:36 +0800

    第7章 对应7A

--- config.py
+++ config.py
@@ -1,13 +1,41 @@
 import os
-CSRF_ENABLED = True
-SECRET_KEY = 'you-will-never-guess'
+basedir = os.path.abspath(os.path.dirname(__file__))
 
-MAIL_SERVER='smtp.163.com'
-MAIL_PORT=25
-MAIL_USE_TLS=True
-MAIL_USERNAME=os.environ.get('MAIL_USERNAME')
-MAIL_PASSWORD=os.environ.get('MAIL_PASSWORD')
 
-FLASKY_MAIL_SUBJECT_PREFIX='[Flasky]'
-FLASKY_MAIL_SENDER='Flasky Admin<mark_ming@163.com>'
-FLASKY_ADMIN='mark_ming@163.com'
+class Config:
+    CSRF_ENABLED = True
+    SECRET_KEY = 'you-will-never-guess'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin<mark_ming@163.com>'
+    FLASKY_ADMIN = 'mark_ming@163.com'
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    MAIL_SERVER = 'smtp.163.com'
+    MAIL_PORT = 25
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DEV_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'TEST_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+
+class ProductionConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        'DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+    'default': DevelopmentConfig
+}
