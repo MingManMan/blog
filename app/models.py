0cbf724bfb3473a12f912fcb62bf660a0538d243
MingManMan <133547@qq.com>
2017-06-01 21:04:27 +0800

    第11章 11.4  11f

--- app/models.py
+++ app/models.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 import hashlib
-
+import bleach
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
@@ -9,7 +9,7 @@ from flask_login import UserMixin, AnonymousUserMixin
 from . import login_manager
 from flask import current_app, request
 from datetime import datetime
-
+from markdown import markdown
 
 class Permission:
     FOLLOW = 0x01
@@ -25,6 +25,13 @@ class Post(db.Model):
     body = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    body_html=db.Column(db.Text)
+
+    @staticmethod
+    def on_changed_body(target,value,oldvalue,initiator):
+        allowed_tags=['a','abbr','acronym','b','blockquote','code','em','i','li','ol','pre','strong','ul','h1','h2','h3','p']
+        target.body_html=bleach.linkify(bleach.clean( markdown(value,output_format='html'),tags=allowed_tags,strip=True))
+
 
     @staticmethod
     def generate_fake(count=100):
@@ -41,6 +48,7 @@ class Post(db.Model):
             db.session.add(p)
             db.session.commit()
 
+db.event.listen(Post.body,'set',Post.on_changed_body)
 
 class Role(db.Model):
     """docstring for Role"""
@@ -68,6 +76,7 @@ class Role(db.Model):
             role.default=roles[r][1]
             db.session.add(role)
         db.session.commit()
+
 
 class User(UserMixin, db.Model):
     __tablename__ = 'users'
