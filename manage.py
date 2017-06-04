b1caf59b17c00f6b7164cd1211369f5d626679c7
MingManMan <133547@qq.com>
2017-06-04 10:05:43 +0800

    第15章覆盖测试部分

--- manage.py
+++ manage.py
@@ -1,5 +1,12 @@
 # coding=utf-8
 import os
+COV=None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV=coverage.coverage(branch=True,include='app/*')
+    COV.start()
+
+
 
 from app import create_app, db
 from app.models import User, Role, Post
@@ -11,20 +18,43 @@ app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
-def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role)
+
 
 @manager.command
-def test():
+def test(coverage=False):
+    if coverage and not os.environ.get('FLASK_COVERAGE'):
+        import sys
+        os.environ['FLASK_COVERAGE']='1'
+        os.execvp(sys.executable,[sys.executable]+sys.argv)
     import unittest
     tests=unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
+    if COV:
+        COV.stop()
+        COV.save()
+        print('coverage summary.')
+        COV.report()
+        basedir=os.path.abspath(os.path.dirname(__file__))
+        covdir=os.path.join(basedir,'tmp/coverage')
+        COV.html_report(directory=covdir)
+        print('HTML version:file://%s/index.html_report'%covdir)
+        COV.erase()
+
+# @manager.command
+# def test():
+#     import unittest
+#     tests=unittest.TestLoader().discover('tests')
+#     unittest.TextTestRunner(verbosity=2).run(tests)
 
 @manager.command
 def rebuild_db(): #重建数据库
     db.drop_all()
     db.create_all()
     Role.insert_roles()
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
